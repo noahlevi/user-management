d3df1b4c6e6b968e3e29f381b8c95366dcbd88dc
madveg009 <koyime9415@jthoven.com>
2023-04-10 16:02:43 +0300

    delete unnecessary

--- user_management/app.py
+++ user_management/app.py
@@ -6,16 +6,12 @@ from user_management.config import init_config, Config
 
 log = logging.getLogger(__name__)
 
-
 init_config()
 # configure_logging()
-log.info('Initialization done')
+log.info("Initialization done")
 
 app = FastAPI(docs_url=Config.c.app.docs_url, title="UserAPI")
 
-
-# app.include_router(healthcheck.router)
 from user_management.api import routes
-app.include_router(routes.router)
 
-# app.add_middleware(TrackRequestsMiddleware)
+app.include_router(routes.router)\
