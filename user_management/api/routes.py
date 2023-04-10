d08ac32ec4de4e7511e197a553dca7d7fd09178d
madveg009 <koyime9415@jthoven.com>
2023-04-10 15:59:31 +0300

    minor changes

--- user_management/api/routes.py
+++ user_management/api/routes.py
@@ -15,7 +15,55 @@ from user_management.utils.enums import UserRoleEnum
 
 log = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users", tags=['users'])
+
+
+@router.post(
+    "/register",
+    description="Register user by id",
+    status_code=status.HTTP_201_CREATED,
+    response_model=User,
+)
+async def register_user(user_schema: UserRegisterRequestSchema):
+    if not (user := await UserModel.insert_user(register_schema=user_schema)):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User with current email is already exists",
+        )
+
+    return user
+
+
+@router.post(
+    "/auth",
+    description="Authenticate user via email and password",
+    status_code=status.HTTP_200_OK,
+    response_model=UserAuthResponseSchema,
+)
+async def auth_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await UserModel.login_user(
+        email=form_data.username, password=form_data.password
+    )
+
+    if user:
+        return UserAuthResponseSchema(
+            access_token=create_access_token(subject=user.email),
+        )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Did not found any user matching provided id",
+        )
+
+
+@router.get(
+    "/me/",
+    description="Get current user data",
+    status_code=status.HTTP_200_OK,
+    response_model=User,
+)
+async def get_me(user: User = Depends(get_current_user)):
+    return user
 
 
 @router.get(
@@ -38,36 +86,6 @@ async def get_user(user_id: str):
             detail="Did not found any user matching provided id",
         )
     return user
-
-
-@router.delete(
-    "{user_id}",
-    description="Get user by id",
-    status_code=status.HTTP_200_OK,
-    response_model=AdminDeleteUserSuccessResponseSchema,
-)
-async def admin_delete_user(
-    user_id: str,
-    user: User = Depends(get_current_user),
-):
-    if user.role != UserRoleEnum.ADMIN.value:
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN, detail="Admin access only"
-            )
-    if not user_id:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Provide user id"
-        )
-
-    deleted = await UserModel.get_by_id(user_id)
-
-    if not deleted:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Did not found any user matching provided id",
-        )
-    return AdminDeleteUserSuccessResponseSchema(user_id=user_id)
 
 
 @router.get(
@@ -122,49 +140,31 @@ async def admin_update_user(
     return user
 
 
-@router.post(
-    "/register",
-    description="Register user by id",
-    status_code=status.HTTP_201_CREATED,
-    response_model=User,
-)
-async def register_user(user_schema: UserRegisterRequestSchema):
-    if not (user := await UserModel.insert_user(register_schema=user_schema)):
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User with current email is already exists",
-        )
-
-    return user
-
-
-@router.post(
-    "/auth",
-    description="Authenticate user via email and password",
+@router.delete(
+    "{user_id}",
+    description="Get user by id",
     status_code=status.HTTP_200_OK,
-    response_model=UserAuthResponseSchema,
+    response_model=AdminDeleteUserSuccessResponseSchema,
 )
-async def auth_user(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await UserModel.login_user(
-        email=form_data.username, password=form_data.password
-    )
-
-    if user:
-        return UserAuthResponseSchema(
-            access_token=create_access_token(subject=user.email),
+async def admin_delete_user(
+    user_id: str,
+    user: User = Depends(get_current_user),
+):
+    if user.role != UserRoleEnum.ADMIN.value:
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Admin access only"
+            )
+    if not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Provide user id"
         )
-    else:
+
+    deleted = await UserModel.get_by_id(user_id)
+
+    if not deleted:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Did not found any user matching provided id",
         )
-
-
-@router.get(
-    "/me/",
-    description="Get current user data",
-    status_code=status.HTTP_200_OK,
-    response_model=User,
-)
-async def get_me(user: User = Depends(get_current_user)):
-    return user
+    return AdminDeleteUserSuccessResponseSchema(user_id=user_id)
